42b35d2511d10c45fe4cb618297ed84883778232
Borja <borjarabadanlp@gmail.com>
2021-10-03 21:38:31 +0200

    Cambio en la request

--- src/main/java/es/inditex/api/bean/SearchRequest.java
+++ src/main/java/es/inditex/api/bean/SearchRequest.java
@@ -3,6 +3,9 @@ package es.inditex.api.bean;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.sql.Timestamp;
@@ -17,11 +20,12 @@ import java.sql.Timestamp;
 @Data
 public class SearchRequest  implements Serializable {
 
-    @NotNull(message = "No puede ser nulo 'productId'")
+    private int brandId;
     private int productId;
+
     @NotNull(message = "No puede ser nulo 'date'")
     private Timestamp date;
-    @NotNull(message = "No puede ser nulo 'brandId'")
-    private int brandId;
+
+
 
 }
